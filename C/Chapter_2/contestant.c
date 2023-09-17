1e0359026cff3e8401c5669758cd78162c75e816
One Purpose <on3purp@gmail.com>
2023-09-17 23:48:26 +0100

    Update contestant.c

--- C/Chapter_2/contestant.c
+++ C/Chapter_2/contestant.c
@@ -7,7 +7,8 @@ int main()
 
 	contestant[0] = 2;
 	contestant[1] = contestant[2];
-	contestant[2] = *choice;
+	contestant[2] = *choice; // I do not know if I make it (* choice = contestant[2])
+				// it gives me 3 instead of 2 like this one
 
 	printf("She chose the contestant %i\n", contestant[2]);
 	
